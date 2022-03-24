67b847545443d08651fc545c7548bb14c0824354
Akanksha <akanksha.sharan09@gmail.com>
2022-03-24 18:01:16 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyApp';
+  constructor(){
+    // setTimeout(() => {
+    //   this.title = "Title changed";
+    // }, 2000);
+  }
 }
